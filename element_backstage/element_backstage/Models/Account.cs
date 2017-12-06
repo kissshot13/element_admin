36e48c7c859a8a2974f9000a37c617f2dafbfb00
kissshot13 <570546847@qq.com>
2017-12-06 22:43:23 +0800

    账户类

---
+++ element_backstage/element_backstage/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace element_backstage.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public bool Del { get; set; }
+    }
+}
